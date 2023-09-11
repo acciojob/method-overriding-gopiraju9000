25d052398fae5f00215b1ef51e870215f9efbfa0
gopiraju000555 <rajumaka911@gmail.com>
2023-09-11 23:35:51 +0530

    return function modified

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,21 +1,25 @@
 package com.driver;
 
+import java.sql.SQLOutput;
+
 public class Main {
     public static void main(String[] args) {
 
         A obj = new B();
-        obj.meth();
+        String ans = obj.meth();
+
+        System.out.println(ans);
     }
   
 }
 class A{
 
-    void meth(){
-        System.out.println("Invoking method from class A");
+    String meth(){
+        return "Invoking method from class A";
     }
 }
 class B extends  A{
-    void  meth(){
-        System.out.println("Method is overridden in Extendend class B");
+    String  meth(){
+        return "Method is overridden in Extendend class B";
     }
 }
