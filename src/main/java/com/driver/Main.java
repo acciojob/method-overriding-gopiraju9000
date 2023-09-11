8f1b3b10a2228839845f1e8b789d6ed01f25a875
gopiraju000555 <rajumaka911@gmail.com>
2023-09-11 23:27:07 +0530

    Method overriding Assignment

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,21 @@
 package com.driver;
 
 public class Main {
+    public static void main(String[] args) {
+
+        A obj = new B();
+        obj.method();
+    }
   
+}
+class A{
+
+    void method(){
+        System.out.println("Invoking method from class A");
+    }
+}
+class B extends  A{
+    void  method(){
+        System.out.println("Method is overridden in Extendend class B");
+    }
 }
